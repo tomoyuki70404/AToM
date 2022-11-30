94a8f2ea6a29e06564869e29316eff8c89a75946
tomoyuki70404 <tomoyuki70404@gmail.com>
2022-12-01 00:33:35 +0900

    add lib

--- integrated/lib/Room.js
+++ integrated/lib/Room.js
@@ -11,9 +11,12 @@ class Room extends EventEmitter{
 	// 既にルームが無い前提
 	static async create({ mediasoupWorker, roomName })
 	{
-		logger.info('create() [roomName:%s]', roomName);
+		console.log('create() [roomName:%s]', roomName);
 		console.log(config)
 		// Router media codecs.
+
+		const peers = new Map()
+
 		const { mediaCodecs } = config.mediasoup.routerOptions;
 
 		// Create a mediasoup Router.
@@ -32,6 +35,7 @@ class Room extends EventEmitter{
 
 		return new Room(
 			{
+				peers,
 				roomName,
 				webRtcServer : mediasoupWorker.appData.webRtcServer,
 				mediasoupRouter,
@@ -41,6 +45,7 @@ class Room extends EventEmitter{
 
 	constructor(
 	{
+		peers,
 		roomName,
 		webRtcServer,
 		mediasoupRouter,
@@ -57,8 +62,8 @@ class Room extends EventEmitter{
 		// @type {Boolean}
 		this._closed = false;
 
-		// Map of broadcasters indexed by id. Each Object has:
-		// - {String} id
+		// Map of peers indexed by id. Each Object has:
+		// - {String} socket.id
 		// - {Object} data
 		//   - {String} displayName
 		//   - {Object} device
@@ -69,7 +74,7 @@ class Room extends EventEmitter{
 		//   - {Map<String, mediasoup.DataProducer>} dataProducers
 		//   - {Map<String, mediasoup.DataConsumers>} dataConsumers
 		// @type {Map<String, Object>}
-		this._broadcasters = new Map();
+		this._peers = new Map();
 
 		// mediasoup WebRtcServer instance.
 		// @type {mediasoup.WebRtcServer}
@@ -84,9 +89,240 @@ class Room extends EventEmitter{
 		this._audioLevelObserver = audioLevelObserver;
 
 		// Handle audioLevelObserver.
-		this._handleAudioLevelObserver();
+		// this._handleAudioLevelObserver();
 
 	}
+
+
+	/**
+	 * Called from server.js upon a WebSocket connection request from a
+	 * browser.
+	 *
+	 * @param {String} socketId - The id of the protoo peer to be created.
+	 * @param {Boolean} consume - Whether this peer wants to consume from others.
+	 * @param {protoo.WebSocketTransport} WebSocketConnection - The associated
+	 *   SocketIO.
+	 */
+	handleWebRtcConnection({socket, consume, webSocketServerConnection})
+	{
+		console.log("socketId",socket.id)
+		socket.emit('connection-success', {
+		  socketId: socket.id,
+		})
+
+		// joinしてきたときのメソッド
+		socket.on('joinRoom',async({ roomName }, callback)=>{
+			console.log('joinRoom ',roomName, ' ', socket.id )
+			const rtpCapabilities = this._mediasoupRouter.rtpCapabilities
+
+			const peerData = {
+				socket:socket,
+				data:
+				{
+					roomName:roomName,
+					// device :
+					// {
+					// 	flag    : 'broadcaster',
+					// 	name    : device.name || 'Unknown device',
+					// 	version : device.version
+					// },
+					rtpCapabilities:rtpCapabilities,
+					transports    : new Map(),
+					producers     : new Map(),
+					consumers     : new Map(),
+					dataProducers : new Map(),
+					dataConsumers : new Map()
+				}
+			};
+
+			this._peers.set(socket.id,peerData)
+			console.log(`this._peers.length: ${this._peers.size}`)
+
+			callback({ rtpCapabilities })
+		})
+
+		socket.on('createWebRtcTransport', async({ isConsume }, callback )=>{
+
+			const peer = this._peers.get(socket.id)
+			const roomName = this._roomName
+
+			console.log("createWebRtcTransport", ` roomName:${roomName} `)
+
+			this.createWebRtcTransport().then(
+				transport => {
+					callback({
+						transportParamfromServer:{
+							id: transport.id,
+							iceParameters: transport.iceParameters,
+							iceCandidates: transport.iceCandidates,
+							dtlsParameters: transport.dtlsParameters,
+						}
+					})
+
+					this.addTransport(socket,transport, isConsume)
+				},
+				error => {
+					logger.error("createWebRtcTransport error:",error)
+				})
+		})
+
+
+		socket.on('transport-connect', ({ dtlsParameters }) => {
+		console.log(`transport-connect ${socket.id}`)
+		  this.getTransport(socket.id).connect({ dtlsParameters })
+		})
+
+		socket.on('transport-produce', async ({ kind, rtpParameters, appData, }, callback) =>{
+			console.log(`transport-produce  appData:${appData}`)
+			const producer = await this.getTransport(socket.id).produce({
+				kind,
+				rtpParameters,
+			})
+			const producerLabel = appData[0].producerLabel
+			const accessLevel = appData[0].accessLevel
+
+			this.addProducer(socket, producer, producerLabel, accessLevel)
+
+			this.informConsumers(socket.id, producer.id, producerLabel, accessLevel)
+
+			console.log(`Producer Id:${producer.id}  producer.kind:${producer.kind}`)
+
+			producer.on('transportclose', () => {
+				console.log(`transport close ${producer}`)
+				// audioLevelObserver.RemoveProducer(producer);
+				producer.close()
+			})
+
+			const peer = this._peers.get(socket.id)
+			const producers = peer.data.producers
+
+			callback({
+			  id: producer.id,
+			  producersExist: producers.length>1 ? true : false,
+			  appData:[producerLabel],
+			})
+		})
+
+		socket.on('getProducers', callback =>{
+			try{
+				console.log(` getProducers  producerParams: ${p}`)
+				let callbackProducerList = []
+
+				let producers = []
+				this._peers.forEach( item => {
+					producers = [
+						...producers,
+						{socketId:item.socket.id, socket:item.socket, producer:item.producer}
+					]
+				});
+				console.log("producers")
+				console.log(producers)
+
+				producers.forEach(producerData => {
+
+					if(producerData.socketId !== socket.id ){
+						callbackProducerList = [
+							...callbackProducerList,
+							{
+								producerId:producerData.producer.id,
+								producerLabel:producerData.producer.producerLabel,
+								accessLevel:producerData.producer.accessLevel
+							},
+						]
+					}
+				})
+				console.log(`getProducers   socket.id${socket.id} callbackProducerList:${callbackProducerList}`)
+
+				callback(callbackProducerList)
+			}catch(error){
+				logger.error("getProducers error",error)
+			}
+		})
+
+		socket.on('transport-recv-connect', async({dtlsParameters, serverConsumerTransportId}) =>{
+			console.log(`transport-recv-connect ${socket.id}`)
+
+			const peer = this._peers.get(socket.id)
+			const consumerTransport = peer.data.transports.find(transportData => (
+				transportData.isConsume && transportData.transport.id == serverConsumerTransportId
+			)).transport
+			await consumerTransport.connect({dtlsParameters})
+		})
+
+		socket.on('consume', async ({rtpCapabilities, remoteProducerId, serverConsumerTransportId }, callback )=>{
+			try{
+				const peer = this._peers.get(socket.id)
+				let consumerTransport = peer.data.transports.find(transportData => (
+					transportData.isConsume && transportData.transport.id == serverConsumerTransportId
+				)).transport
+
+				if(this._mediasoupRouter.canConsume({
+					producerId: remoteProducerId,
+					rtpCapabilities
+				})){
+					const consumer = await consumerTransport.consume({
+						producerId: remoteProducerId,
+						rtpcapabilities,
+						paused:true,
+					})
+
+					consumer.on('transportclose',() =>{
+						console.log('transport close from consumer')
+					})
+
+					consumer.on('producerclose', () =>{
+						console.log(`producer of consumer close`)
+
+						socket.emit('producer-closed', { remoteProducerId })
+
+						consumerTransport.close([])
+
+						const peer = this._peers.get(socket.id)
+						peer.data.transports.delete(socket.id)
+						consumer.close()
+						peer.data.consumers.delete(socket.id)
+					})
+
+					this.addConsumer(consumer, roomName)
+
+					const consumerParams = {
+						id:consumer.id,
+						producerId:remoteProducerId,
+						kind:consumer.kind,
+						rtpParameters: consumer.rtpParameters,
+						serverconsumerId: consumer.id,
+					}
+
+					callback({ consumerParams })
+				}
+			}catch( error ){
+				console.log(`consume error ${error}`)
+
+				callback({
+					consumerParams:{
+						error:error
+					}
+				})
+			}
+		})
+
+		socket.on('consumer-resume', async ({ serverConsumerId }) => {
+			console.log(`consumer resume ${socket.id}`)
+
+			const peer = this._peers.get(socket.id)
+			const { consumer } = peer.data.consumers.find(consumerData => consumerData.consumer.id === serverConsumerId)
+			await consumer.resume()
+		})
+
+		socket.on('disconnect', () => {
+			//peers内のsocketデータを削除
+			this._peers.delete(socket.id)
+
+			console.log('disconnect End', socket.id)
+
+		})
+	}
+
 
 	_handleAudioLevelObserver()
 	{
@@ -140,67 +376,161 @@ class Room extends EventEmitter{
 
 	}
 
-	/**
-	 * Called from server.js upon a WebSocket connection request from a
-	 * browser.
-	 *
-	 * @param {String} socketId - The id of the protoo peer to be created.
-	 * @param {Boolean} consume - Whether this peer wants to consume from others.
-	 * @param {protoo.WebSocketTransport} WebSocketConnection - The associated
-	 *   SocketIO.
-	 */
-	handleWebRtcConnection({socket, consume, webSocketServerConnection})
-	{
-		logger.info("socketId",socket.id)
-		socket.emit('connection-success', {
-		  socketId: socket.id,
-		})
-
-		socket.data.consume = consume;
-		socket.data.joined = false;
-		socket.data.displayName = undefined;
-		socket.data.device = undefined;
-		socket.data.rtpCapabilities = undefined;
-		socket.data.sctpCapabilities = undefined;
-
-		socket.data.transports = new Map();
-		socket.data.producers = new Map();
-		socket.data.consumers = new Map();
-		socket.data.dataProducers = new Map();
-		socket.data.dataConsumers = new Map();
-
-
-
-
-
-
-		// joinしてきたときのメソッド
-		socket.on('joinRoom',async({ roomName }, callback)=>{
-			logger.info('joinRoom ',roomName, ' ', socket.id )
-
-			const rtpCapabilities = this._mediasoupRouter.rtpCapabilities
-
-			callback({ rtpCapabilities })
-		})
-
-		socket.on('disconnet',()=>{
-			logger.info('disconnect', socket.id)
-			socket.data.consumers = removeItems(socket.data.consumers,socket.id);
-			socket.data.producers = removeItems(socket.data.producers,socket.id);
-			socket.data.transports = removeItems(socket.data.transports,socket.id);
-
-		})
-
-
-	removeItems(items, socketId, type) =>{
-		items.delete(sockeId)
-	    return items
+	getTransport(socketId){
+		const peer = this._peers.get(socketId)
+		// const [producerTransport] = peer.data.transports.filter(transportData => transportData.socketId === socketId && !transportData.isConsume)
+		let producerTransport = []
+		peer.data.transports.forEach(item => {
+			if(item.socket.id === socketId && ! item.isConsume){
+				producerTransport = item
+			}
+		});
+		console.log(`getTransport ${socketId}` )
+		return producerTransport.transport
 	}
 
+	addTransport(socket, transport, isConsume){
+		console.log("addTransport", ` transport:${transport} `, ` isConsume:${isConsume}`)
+
+		const peer = this._peers.get(socket.id)
+		const transportData ={
+			socket:socket,
+			transport:transport,
+			isConsume:isConsume
+		}
+		peer.data.transports.set(transport.id, transportData )
+
+		console.log(`transports.length: ${peer.data.transports.size}`)
+	}
+
+	addProducer(socket, producer, producerLabel, accessLevel){
+		console.log(`addProducer  producer:${producer}  producerLabel:${producerLabel} accessLevel:${accessLevel}`)
+
+		const peer = this._peers.get(socket.id)
+		const producerData ={
+			socket:socket,
+			producer:producer,
+			producerLabel:producerLabel,
+			accessLevel:accessLevel
+		}
+		peer.data.producers.set(producer.id, producerData )
+	}
+
+	addConsumer(socket, consumer){
+		console.log(`addConsumer  socket:${socket}  consumer:${consumer}`)
+
+		const peer = this._peers.get(socket.id)
+		const consumerData = {
+			socket:socket,
+			consumer:consumer
+		}
+		peer.data.consumers.set(consumer.id, consumerData )
+
+		console.log(`addConsumer:${consumers.length}`)
+	}
+
+	informConsumers( socketId, producerId, producerLabel, accessLevel ){
+		console.log(`informConsumers, socket.id:${socketId} producerId:${producerId} producerLabel:${producerLabel} accessLevel:${accessLevel}`)
+
+		let consumers = []
+		this._peers.forEach( item => {
+			consumers = [
+				...consumers,
+				{socketId:item.socket.id, socket:item.socket, consumer:item.consumer}
+			]
+		});
+
+		let producers = []
+		this._peers.forEach( item => {
+			producers = [
+				...producers,
+				{socketId:item.socket.id, socket:item.socket, producer:item.producer}
+			]
+		});
+
+		// producerが１つ以上ある場合
+		if(consumers.length == 0 && peer.data.producers.length > 0){
+
+			console.log('inform one consumer')
+
+			for(let key of this._peers.keys()){
+				if(key != socketId){
+					const peer = this._peers.get(key)
+					const firstConsumerSocket = peer.socket
+					console.log('first consumer : get new-producer')
+					firstConsumerSocket.emit('new-producer',{
+						producerId:producerid,
+						producerLabel:producerLabel,
+						accessLevel:accessLevel
+					})
+				}
+			}
+		}else{ // consumerが１番最初にいる場合、getproducerを呼べないのでその場合の処理
+
+			//１人のクライアント先に複数のconsumerがあるとき,すべてにinformConsumersが送られるので、重複を回避するリスト
+			let consumerSocketIdList = []
+
+			let consumers = []
+			this._peers.forEach( item => {
+				consumers = [
+					...consumers,
+					{socketId:item.socket.id, socket:item.socket, consumer:item.consumer}
+				]
+			});
+
+			consumers.forEach(consumerData => {
+				// consumer自身のsocketにinformしないように排除
+				if(consumerData.socketId !== socketId){
+
+					if(consumerSocketIdList.includes(consumerData.socketId) == false){
+						console.log(`consumerData socketId: ${consumerData.socketId}  socketId: ${socketId}`)
+
+						consumerData.socket.emit('new-producer',{
+							producerId:producerId,
+							producerLabel:producerLabel,
+							accessLevel:accessLevel,
+						})
+						consumerSocketIdList.push(consumerData.socketId)
+					}else{
+						logger.error("add consumer socketId pass")
+					}
+				}
+			});
+		}
+	}
+
+	async createWebRtcTransport(){
+		return new Promise(async (resolve, reject)=>{
+			try{
+				const webRtcTransport_options = {
+			        listenIps: [
+			          {
+			            ip: '192.168.10.113', // replace with relevant IP address
+			            // announcedIp: '10.0.0.115',
+			          }
+			        ],
+			        enableUdp: true,
+			        enableTcp: true,
+			        preferUdp: true,
+			    }
+
+				let transport = await this._mediasoupRouter.createWebRtcTransport(webRtcTransport_options)
+				console.log(`transport id: ${transport.id}`)
 
 
-
-
+				transport.on('dtlsstatechange', dtlsState =>{
+					if (dtlsState === 'closed') {
+			        	transport.close()
+			        }
+				})
+				transport.on('close', () => {
+		          console.log('transport closed')
+		        })
+		        resolve(transport)
+			}catch(error){
+				reject(error)
+			}
+		})
 	}
 
 
